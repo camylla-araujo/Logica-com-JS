8354d6d949031c088805c8f3a5686035420c2224
camylla-araujo <camylla.a_@hotmail.com>
2022-09-20 21:53:17 -0300

    Corrigindo

--- Condicional/Quest11/.js
+++ Condicional/Quest11/.js
@@ -6,3 +6,30 @@
 // 2 À vista no cartão de crédito, recebe 15% de desconto
 // 3 Em duas vezes, preço normal de etiqueta sem juros
 // 4 Em duas vezes, preço normal de etiqueta mais juros de 10%
+
+let preco = 450
+let Dinheiro = false
+let Cheque = false
+let Credito1X = false
+let Credito2X = true
+let Credito3X = false
+
+let valorTotal 
+
+      if (Dinheiro == true) {
+         valorTotal = preco - (valor * 0.10);
+}
+      if (Cheque == true) {
+         valorTotal = preco - (valor * 0.10);
+}
+      if (Credito1X == true) {
+         valorTotal = preco - (valor * 0.15);
+}
+      if (Credito2X == true) {
+         valorTotal = preco;
+}
+      if (Credito3X == true) {
+         valorTotal = preco + (valor * 0.10);
+}
+
+         console.log(valorTotal)
